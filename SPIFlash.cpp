021513b0d29aa654363aee906cbf90e3ba81f417
BarryPSmith <smith.barryp@gmail.com>
2020-05-18 21:10:33 -0700

    Sped up SPI

--- SPIFlash.cpp
+++ SPIFlash.cpp
@@ -67,7 +67,8 @@ void SPIFlash::select() {
   // set FLASH SPI settings
   SPI.setDataMode(SPI_MODE0);
   SPI.setBitOrder(MSBFIRST);
-  SPI.setClockDivider(SPI_CLOCK_DIV4); //decided to slow down from DIV2 after SPI stalling in some instances, especially visible on mega1284p when RFM69 and FLASH chip both present
+  
+  SPI.setClockDivider(SPI_CLOCK_DIV2); //BS: We're running at 1MHz. No need for more divider than necessary.
 #endif
   digitalWrite(_slaveSelectPin, LOW);
 }
